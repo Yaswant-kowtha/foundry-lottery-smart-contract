3be1dcd2043afb2c2903f127210de8cdae7e4a3b
Yaswanth Kowtha <ykowtha@gmail.com>
2024-08-23 15:41:30 +0530

    Emit event on player joining Raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
@@ -8,13 +29,27 @@ pragma solidity ^0.8.13;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /** Errors */
+    error Raffle_SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /** Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value < i_entranceFee, "Not enough ETH sent!");
+        if(msg.value < i_entranceFee) {
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
